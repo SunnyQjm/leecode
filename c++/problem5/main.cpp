72d6d952be134bb773b6acf60261107c1b2f995e
SunnyQjm <qjm253@pku.edu.cn>
2020-01-28 18:36:29 +0800

    modify: leecode problem 5 (C++) 动态规划解法

--- c++/problem5/main.cpp
+++ c++/problem5/main.cpp
@@ -20,6 +20,7 @@ string longestPalindrome(string s) {
     int resultRightIndex = 0;
     for(int i = 0; i < length; i++) {
         for(int j = i; j < length; j++) {
+            // 剪枝，比当前最长串短或者相等的不考虑
             if(j - i > resultRightIndex - resultLeftIndex && checkIsPalindrome(s, i, j)) {
                 resultLeftIndex = i;
                 resultRightIndex = j;
@@ -29,8 +30,52 @@ string longestPalindrome(string s) {
     return s.substr(resultLeftIndex, resultRightIndex - resultLeftIndex + 1);
 }
 
+/*****************************************
+ * 动态规划法
+ *****************************************/
+
+string longestPalindrome2(string s) {
+    int length = s.length();
+    
+    // 用来记录动态规划过程中的计算结果，减少重复的计算
+    // 题目限定字符串的长度不超过1000，故设置二维数组的大小为1000x1000
+    bool recordArr[1000][1000];
+    for(int i = 0; i < length; i++) {
+        recordArr[i][i] = true;
+    }
+    
+    int resultLeftIndex = 0;
+    int resultRightIndex = 0;
+    
+    for(int i = 0; i < length - 1; i++) {
+        recordArr[i][i + 1] = (s.at(i) == s.at(i + 1));
+        if(recordArr[i][i + 1]) {
+            resultLeftIndex = i;
+            resultRightIndex = i + 1;
+        }
+    }
+
+    bool find;
+    for(int i = 2; i < length; i++) {
+        find = false;
+        for(int j = 0; j < length - i; j++) {
+            recordArr[j][j + i] = (s.at(j) == s.at(j + i) && recordArr[j + 1][j + i - 1]);
+            if(recordArr[j][j + i]) {  // 有长度为(i + 1)的回文子串
+                resultLeftIndex = j;
+                resultRightIndex = j + i;
+                find = true;
+            }
+        }
+    }
+
+    return s.substr(resultLeftIndex, resultRightIndex - resultLeftIndex + 1);
+}
+
 int main() {
     cout << longestPalindrome("babad") << endl;
     cout << longestPalindrome("cbbd") << endl;
+    cout << longestPalindrome2("babad") << endl;
+    cout << longestPalindrome2("cbbd") << endl;
+    cout << longestPalindrome2("abcba") << endl;
     return 0;
 }
