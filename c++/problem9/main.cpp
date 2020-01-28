869d0f23739781ea0ba977115201533176a1cf9d
SunnyQjm <qjm253@pku.edu.cn>
2020-01-28 22:25:19 +0800

    add: leecode problem 9 (C++)

---
+++ c++/problem9/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPalindrome(int x) {
+    if(x < 0) {
+        return false;
+    }
+    string s = to_string(x);
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+        if(s.at(i) != s.at(j))
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    cout << isPalindrome(121) << endl;
+    cout << isPalindrome(-121) << endl;
+    cout << isPalindrome(10) << endl;
+    return 0;
+}
