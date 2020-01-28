c5d48e0c58241d3a0f88cb1eed7bf36a426b82c1
SunnyQjm <qjm253@pku.edu.cn>
2020-01-28 21:36:47 +0800

    modify: leecode problem 7 (C++) 直接数学计算解法

--- c++/problem7/main.cpp
+++ c++/problem7/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int INT_MAX_VAL = 2147483647;
 int INT_MIN_VAL = -2147483648;
-long reverse(int x) {
+int reverse(int x) {
     if(x == INT_MIN_VAL) {
         return 0;
     }
@@ -31,7 +31,38 @@ long reverse(int x) {
     return result;
 }
 
+/////////////////////////////////////////////////
+/////// 直接数学计算解法
+/////////////////////////////////////////////////
+int reverse2(int x) {
+    if(x == INT_MIN_VAL) {
+        return 0;
+    }
+    bool isNegtive = x < 0 ? true : false;
+    x = x < 0 ? -x : x;
+    long result = 0;
+    while(x > 0) {
+        int tmp = x % 10;
+        x /= 10;
+        result = result * 10 + tmp;
+    }
+    if(isNegtive) {
+        result = -result;
+    }
+    if(result > INT_MAX_VAL || result < INT_MIN_VAL) {
+        return 0;
+    }
+    return result;
+}
+
+
 int main() {
+    cout << reverse(123) << endl;
+    cout << reverse(-123) << endl;
+    cout << reverse(120) << endl;
+    cout << reverse(1147483647) << endl;
+    
+    cout << "==============" << endl;
     cout << reverse(123) << endl;
     cout << reverse(-123) << endl;
     cout << reverse(120) << endl;
