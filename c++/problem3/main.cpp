fa34038dedae1da31115f34c048453865627c1df
SunnyQjm <qjm253@pku.edu.cn>
2020-01-31 16:20:15 +0800

    modify: leetcode problem 3 (C++) use gtest to test

--- c++/problem3/main.cpp
+++ c++/problem3/main.cpp
@@ -1,122 +1,63 @@
 #include <iostream>
+#include <gtest/gtest.h>
+#include "solution.cpp"
 
 using namespace std;
 
-/*************************************************
- * 暴力解法
- **************************************************/
+Solution solution;
 
-bool judgeIsExists(const string& s, int start, int end, char target) {
-    for(int i = start; i <= end; i++) {
-        if(s.at(i) == target) {
-            return true;
-        }
-    }
-    return false;
+TEST(Leecode3_lengthOfLongestSubstring, test0) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring("abcabcbb"), 3);
 }
 
-int lengthOfLongestSubstring(const string& s) {
-    int max = 0;
-    bool change = false;
-    for(int i = 0; i < s.size(); i++) {
-        for(int j = i + 1; j < s.size(); j++) {
-            if(judgeIsExists(s, i, j - 1, s.at(j))) {
-                max = j - i > max ? j - i : max;
-                change = true;
-                break;
-            }
-        }
-        if(!change) {
-            max = s.size() - i > max ? s.size() - i : max;
-        } else {
-            change = false;
-        }
-    }
-    return max;
+TEST(Leecode3_lengthOfLongestSubstring, test1) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring("bbbbb"), 1);
 }
 
-/*************************************************
- * 双指针滑动窗口剪枝法
- *************************************************/
-int judgeIsExists2(const string& s, int start, int end, char target) {
-    for(int i = start; i <= end; i++) {
-        if(s.at(i) == target) {
-            return i;
-        }
-    }
-    return -1;
+TEST(Leecode3_lengthOfLongestSubstring, test2) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring("pwwkew"), 3);
+}
+
+TEST(Leecode3_lengthOfLongestSubstring, test3) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring(" "), 1);
 }
 
 
-int lengthOfLongestSubstring2(const string& s) {
-    if(s.size () <= 1) {
-        return s.size();
-    }
-    int left = 0;
-    int right = 1;
-    int max = 0;
-    int repeatIndex = -1;
-    while(left < s.size() - 1 && right < s.size()) {
-        if((repeatIndex = judgeIsExists2(s, left, right - 1, s.at(right))) >= 0) {
-            max = right - left > max ? right - left : max;
-            left = repeatIndex + 1;
-        } else {
-            right++;
-        }
-    }
-    max = right - left > max ? right - left : max;
-    return max;
+TEST(Leecode3_lengthOfLongestSubstring2, test0) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring("abcabcbb"), 3);
 }
 
-/***************************************************
- * HashMap 优化解法
- * ps: 这边数据域取值的大小固定，扩展ASCII最多有256个，用265个bool数组模拟hashmap即可
- ****************************************************/
-
-int lengthOfLongestSubstring3(const string& s) {
-    if(s.size () <= 1) {
-        return s.size();
-    }
-    int map[256];
-    for(int i = 0; i < 256; i++) {
-        map[i] = -1;
-    }
-    int left = 0;
-    int right = 1;
-    int max = 0;
-    int repeatIndex = -1;
-    map[s.at(left)] = left;
-    while(left < s.size() - 1 && right < s.size()) {
-        if((repeatIndex = map[s.at(right)]) >= 0) {
-            max = right - left > max ? right - left : max;
-            for(int i = left; i <= repeatIndex; i++) {
-                map[s.at(i)] = -1;
-            }
-            left = repeatIndex + 1;
-        } else {
-            map[s.at(right)] = right;
-            right++;
-        }
-    }
-    max = right - left > max ? right - left : max;
-    return max;
+TEST(Leecode3_lengthOfLongestSubstring2, test1) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring("bbbbb"), 1);
 }
 
-int main() {
-    cout << lengthOfLongestSubstring("abcabcbb") << endl;
-    cout << lengthOfLongestSubstring("bbbbb") << endl;
-    cout << lengthOfLongestSubstring("pwwkew") << endl;
-    cout << lengthOfLongestSubstring(" ") << endl;
+TEST(Leecode3_lengthOfLongestSubstring2, test2) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring("pwwkew"), 3);
+}
 
-    
-    /* cout << lengthOfLongestSubstring2("abcabcbb") << endl; */
-    /* cout << lengthOfLongestSubstring2("bbbbb") << endl; */
-    /* cout << lengthOfLongestSubstring2("pwwkew") << endl; */
-    /* cout << lengthOfLongestSubstring2(" ") << endl; */
-    
-    cout << lengthOfLongestSubstring3("abcabcbb") << endl;
-    cout << lengthOfLongestSubstring3("bbbbb") << endl;
-    cout << lengthOfLongestSubstring3("pwwkew") << endl;
-    cout << lengthOfLongestSubstring3(" ") << endl;
-    return 0;
+TEST(Leecode3_lengthOfLongestSubstring2, test3) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring(" "), 1);
+}
+
+TEST(Leecode3_lengthOfLongestSubstring3, test0) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring("abcabcbb"), 3);
+}
+
+TEST(Leecode3_lengthOfLongestSubstring3, test1) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring("bbbbb"), 1);
+}
+
+TEST(Leecode3_lengthOfLongestSubstring3, test2) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring("pwwkew"), 3);
+}
+
+TEST(Leecode3_lengthOfLongestSubstring3, test3) {
+    EXPECT_EQ(solution.lengthOfLongestSubstring(" "), 1);
+}
+
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
