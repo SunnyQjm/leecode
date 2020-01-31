73423b299673bad73b6a1a5026d0c633ae884068
SunnyQjm <qjm253@pku.edu.cn>
2020-01-31 16:54:45 +0800

    modify: leetcode problem 9 (C++) use gtest to test

--- c++/problem8/main.cpp
+++ c++/problem8/main.cpp
@@ -1,73 +1,31 @@
 #include <iostream>
+#include <gtest/gtest.h>
+#include "solution.cpp"
 
 using namespace std;
+Solution solution;
 
-int INT_MAX_VAL = 2147483647;
-int INT_MIN_VAL = -2147483648;
-
-int myAtoi(string str) {
-    if(str.size() == 0) {
-        return 0;
-    }
-    bool find = false;
-    long result = 0;
-    bool isNegtive = false;
-    bool isRecordMark = false;
-    bool isRecodZero = false;
-    for(const char& c : str) {
-        if(!find) {
-            if(c == ' ') {
-                if(isRecordMark) {
-                    return 0;
-                }
-                continue;
-            } else if (c == '0') {
-                isRecordMark = true;
-                continue;
-            } else if (c == '+') {
-                if(isRecordMark) {
-                    return 0;
-                }
-                isRecordMark = true;
-            } else if (c == '-') {
-                if(isRecordMark) {
-                    return 0;
-                }
-                isNegtive = true;
-                isRecordMark = true;
-            } else if(c >= '1' && c <= '9') {
-                find = true;
-                result = (c - '0');
-            } else {
-                return 0;
-            }
-        } else {
-            if(c >= '0' && c <= '9') {
-                result = result * 10 + (c - '0');
-                if(result > INT_MAX_VAL) {
-                    break;
-                }
-            } else {
-                break;
-            }
-        }
-    }
-    if(isNegtive) {
-        result = -result;
-    }
-    if(result > INT_MAX_VAL) {
-        return INT_MAX_VAL;
-    } else if(result < INT_MIN_VAL) {
-        return INT_MIN_VAL;
-    } else {
-        return result;
-    }
+TEST(Leetcode8_myAtoi, test0) {
+    EXPECT_EQ(solution.myAtoi("42"), 42);
 }
-int main() {
-    cout << myAtoi("42") << endl;
-    cout << myAtoi("     -42") << endl;
-    cout << myAtoi("4139 with sdfa ") << endl;
-    cout << myAtoi("df42") << endl;
-    cout << myAtoi("2147483649") << endl;
-    return 0;
+
+TEST(Leetcode8_myAtoi, test1) {
+    EXPECT_EQ(solution.myAtoi("      -42"), -42);
+}
+
+TEST(Leetcode8_myAtoi, test2) {
+    EXPECT_EQ(solution.myAtoi("4239 with sdfa"), 4239);
+}
+
+TEST(Leetcode8_myAtoi, test3) {
+    EXPECT_EQ(solution.myAtoi("df42"), 0);
+}
+
+TEST(Leetcode8_myAtoi, test4) {
+   EXPECT_EQ(solution.myAtoi("2147483649"), 2147483647);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
---
+++ c++/problem8/solution.cpp
@@ -0,0 +1,68 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int INT_MAX_VAL = 2147483647;
+    int INT_MIN_VAL = -2147483648;
+
+    int myAtoi(string str) {
+        if(str.size() == 0) {
+            return 0;
+        }
+        bool find = false;
+        long result = 0;
+        bool isNegtive = false;
+        bool isRecordMark = false;
+        bool isRecodZero = false;
+        for(const char& c : str) {
+            if(!find) {
+                if(c == ' ') {
+                    if(isRecordMark) {
+                        return 0;
+                    }
+                    continue;
+                } else if (c == '0') {
+                    isRecordMark = true;
+                    continue;
+                } else if (c == '+') {
+                    if(isRecordMark) {
+                        return 0;
+                    }
+                    isRecordMark = true;
+                } else if (c == '-') {
+                    if(isRecordMark) {
+                        return 0;
+                    }
+                    isNegtive = true;
+                    isRecordMark = true;
+                } else if(c >= '1' && c <= '9') {
+                    find = true;
+                    result = (c - '0');
+                } else {
+                    return 0;
+                }
+            } else {
+                if(c >= '0' && c <= '9') {
+                    result = result * 10 + (c - '0');
+                    if(result > INT_MAX_VAL) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        if(isNegtive) {
+            result = -result;
+        }
+        if(result > INT_MAX_VAL) {
+            return INT_MAX_VAL;
+        } else if(result < INT_MIN_VAL) {
+            return INT_MIN_VAL;
+        } else {
+            return result;
+        }
+    }
+};
