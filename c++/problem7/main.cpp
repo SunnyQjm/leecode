45c76b474822d49ce5988370bb87e005f0e152a7
SunnyQjm <qjm253@pku.edu.cn>
2020-01-31 16:47:53 +0800

    modify: leetcode problem 7 (C++) use gtest to test

--- c++/problem7/main.cpp
+++ c++/problem7/main.cpp
@@ -1,71 +1,43 @@
 #include <iostream>
-#include <string>
-#include <sstream>
+#include <gtest/gtest.h>
+#include "solution.cpp"
+
 using namespace std;
+Solution solution;
 
-/////////////////////////////////////////////////
-/////// 字符串解法（先转成字符串，再转为int）
-////////////////////////////////////////////////
-
-int INT_MAX_VAL = 2147483647;
-int INT_MIN_VAL = -2147483648;
-int reverse(int x) {
-    if(x == INT_MIN_VAL) {
-        return 0;
-    }
-    bool isNegtive = x < 0 ? true : false;
-    x = x < 0 ? -x : x;
-    string tmp = to_string(x);
-    stringstream ss;
-    if(isNegtive) {
-        ss << "-";
-    }
-    for(int i = tmp.length() - 1; i >= 0; i--) {
-        ss << tmp.at(i);
-    }
-    long result = 0;
-    ss >> result;
-    if(result > INT_MAX_VAL || result < INT_MIN_VAL) {
-        return 0;
-    }
-    return result;
+TEST(Leetcode7_reverse, test0) {
+    EXPECT_EQ(solution.reverse(123), 321);
 }
 
-/////////////////////////////////////////////////
-/////// 直接数学计算解法
-/////////////////////////////////////////////////
-int reverse2(int x) {
-    if(x == INT_MIN_VAL) {
-        return 0;
-    }
-    bool isNegtive = x < 0 ? true : false;
-    x = x < 0 ? -x : x;
-    long result = 0;
-    while(x > 0) {
-        int tmp = x % 10;
-        x /= 10;
-        result = result * 10 + tmp;
-    }
-    if(isNegtive) {
-        result = -result;
-    }
-    if(result > INT_MAX_VAL || result < INT_MIN_VAL) {
-        return 0;
-    }
-    return result;
+TEST(Leetcode7_reverse, test1) {
+    EXPECT_EQ(solution.reverse(-123), -321);
 }
 
+TEST(Leetcode7_reverse, test2) {
+    EXPECT_EQ(solution.reverse(120), 21);
+}
 
-int main() {
-    cout << reverse(123) << endl;
-    cout << reverse(-123) << endl;
-    cout << reverse(120) << endl;
-    cout << reverse(1147483647) << endl;
-    
-    cout << "==============" << endl;
-    cout << reverse(123) << endl;
-    cout << reverse(-123) << endl;
-    cout << reverse(120) << endl;
-    cout << reverse(1147483647) << endl;
-    return 0;
+TEST(Leetcode7_reverse, test3) {
+    EXPECT_EQ(solution.reverse(1147483647), 0);
+}
+
+TEST(Leetcode7_reverse2, test0) {
+    EXPECT_EQ(solution.reverse2(123), 321);
+}
+
+TEST(Leetcode7_reverse2, test1) {
+    EXPECT_EQ(solution.reverse2(-123), -321);
+}
+
+TEST(Leetcode7_reverse2, test2) {
+    EXPECT_EQ(solution.reverse2(120), 21);
+}
+
+TEST(Leetcode7_reverse2, test3) {
+    EXPECT_EQ(solution.reverse2(1147483647), 0);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
---
+++ c++/problem7/solution.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <sstream>
+using namespace std;
+
+class Solution {
+public:
+    /////////////////////////////////////////////////
+    /////// 字符串解法（先转成字符串，再转为int）
+    ////////////////////////////////////////////////
+
+    int INT_MAX_VAL = 2147483647;
+    int INT_MIN_VAL = -2147483648;
+    int reverse(int x) {
+        if(x == INT_MIN_VAL) {
+            return 0;
+        }
+        bool isNegtive = x < 0 ? true : false;
+        x = x < 0 ? -x : x;
+        string tmp = to_string(x);
+        stringstream ss;
+        if(isNegtive) {
+            ss << "-";
+        }
+        for(int i = tmp.length() - 1; i >= 0; i--) {
+            ss << tmp.at(i);
+        }
+        long result = 0;
+        ss >> result;
+        if(result > INT_MAX_VAL || result < INT_MIN_VAL) {
+            return 0;
+        }
+        return result;
+    }
+
+    /////////////////////////////////////////////////
+    /////// 直接数学计算解法
+    /////////////////////////////////////////////////
+    int reverse2(int x) {
+        if(x == INT_MIN_VAL) {
+            return 0;
+        }
+        bool isNegtive = x < 0 ? true : false;
+        x = x < 0 ? -x : x;
+        long result = 0;
+        while(x > 0) {
+            int tmp = x % 10;
+            x /= 10;
+            result = result * 10 + tmp;
+        }
+        if(isNegtive) {
+            result = -result;
+        }
+        if(result > INT_MAX_VAL || result < INT_MIN_VAL) {
+            return 0;
+        }
+        return result;
+    }
+
+
+};
