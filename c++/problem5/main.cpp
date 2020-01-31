eaf54928744df6ff9268176a498d3d78a893e192
SunnyQjm <qjm253@pku.edu.cn>
2020-01-31 16:35:32 +0800

    modify: leetcode problem 5 (C++) use gtest to test

--- c++/problem5/main.cpp
+++ c++/problem5/main.cpp
@@ -1,78 +1,33 @@
 #include <iostream>
+#include <gtest/gtest.h>
+#include "solution.cpp"
 
 using namespace std;
 
+Solution solution;
 
-/***************************************
- * 暴力破解实现
- ***************************************/
-bool checkIsPalindrome(const string& s, int start, int end) {
-    for(; start < end; start++, end--) {
-        if(s.at(start) != s.at(end))
-            return false;
-    }
-    return true;
+TEST(Leetcode5_longestPalindrome, test0) {
+    EXPECT_EQ(solution.longestPalindrome("babad"), "bab");
 }
 
-string longestPalindrome(string s) {
-    int length = s.length();
-    int resultLeftIndex = 0;
-    int resultRightIndex = 0;
-    for(int i = 0; i < length; i++) {
-        for(int j = i; j < length; j++) {
-            // 剪枝，比当前最长串短或者相等的不考虑
-            if(j - i > resultRightIndex - resultLeftIndex && checkIsPalindrome(s, i, j)) {
-                resultLeftIndex = i;
-                resultRightIndex = j;
-            }
-        }
-    }
-    return s.substr(resultLeftIndex, resultRightIndex - resultLeftIndex + 1);
+TEST(Leetcode5_longestPalindrome, test1) {
+    EXPECT_EQ(solution.longestPalindrome("cbbd"), "bb");
 }
 
-/*****************************************
- * 动态规划法
- *****************************************/
-
-string longestPalindrome2(string s) {
-    int length = s.length();
-    
-    // 用来记录动态规划过程中的计算结果，减少重复的计算
-    // 题目限定字符串的长度不超过1000，故设置二维数组的大小为1000x1000
-    bool recordArr[1000][1000];
-    for(int i = 0; i < length; i++) {
-        recordArr[i][i] = true;
-    }
-    
-    int resultLeftIndex = 0;
-    int resultRightIndex = 0;
-    
-    for(int i = 0; i < length - 1; i++) {
-        recordArr[i][i + 1] = (s.at(i) == s.at(i + 1));
-        if(recordArr[i][i + 1]) {
-            resultLeftIndex = i;
-            resultRightIndex = i + 1;
-        }
-    }
-
-    for(int i = 2; i < length; i++) {
-        for(int j = 0; j < length - i; j++) {
-            recordArr[j][j + i] = (s.at(j) == s.at(j + i) && recordArr[j + 1][j + i - 1]);
-            if(recordArr[j][j + i]) {  // 有长度为(i + 1)的回文子串
-                resultLeftIndex = j;
-                resultRightIndex = j + i;
-            }
-        }
-    }
-
-    return s.substr(resultLeftIndex, resultRightIndex - resultLeftIndex + 1);
+TEST(Leetcode5_longestPalindrome2, test0) {
+    EXPECT_EQ(solution.longestPalindrome2("babad"), "aba");
 }
 
-int main() {
-    cout << longestPalindrome("babad") << endl;
-    cout << longestPalindrome("cbbd") << endl;
-    cout << longestPalindrome2("babad") << endl;
-    cout << longestPalindrome2("cbbd") << endl;
-    cout << longestPalindrome2("abcba") << endl;
-    return 0;
+TEST(Leetcode5_longestPalindrome2, test1) {
+    EXPECT_EQ(solution.longestPalindrome2("cbbd"), "bb");
 }
+
+TEST(Leetcode5_longestPalindrome2, test2) {
+    EXPECT_EQ(solution.longestPalindrome2("abcba"), "abcba");
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
