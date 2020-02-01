b082ea9c191608f7b9c040fcaf443134d5bd2768
SunnyQjm <qjm253@pku.edu.cn>
2020-02-01 11:47:04 +0800

    leetcode problem 15 (C++) 添加注释

--- c++/problem15/solution.cpp
+++ c++/problem15/solution.cpp
@@ -7,6 +7,7 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        // 处理数组长度小于等于3的特殊情况
         if(nums.size() < 3 ) {
             return {};
         } else if (nums.size() == 3) {
@@ -18,16 +19,20 @@ public:
         }
         // 将数组按升序排序
         sort(nums.begin(), nums.end(), less<int>());
+        
+        // 用来记录第一个数的上一次取值
         int last = 0;
         int left, right, lastLeft, lastRight, tmp;
         vector<vector<int>> result;
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] >= 0) {
+                // 处理3个以上连续0出现的特殊情况
                 if(nums[i] == 0 && i < nums.size() - 2 && nums[i + 1] == 0 && nums[i + 2] == 0) {
                     result.push_back({0, 0, 0});
                 }
                 break;
             }
+            // 跳过重复
             if(nums[i] == last) {
                 continue;
             }
@@ -35,6 +40,8 @@ public:
             right = nums.size() - 1;
             lastLeft = nums[left] - 1;
             lastRight = nums[right] + 1;
+
+            // 双指针法处理 two sum
             while(left < right) {
                 if(nums[left] == lastLeft) {
                     left++;
